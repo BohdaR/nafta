86cefeddf5906d5aebda64a0c10eb66b1190e2ee
BohdaR <bohdanshushval@gmail.com>
2022-12-06 11:49:23 +0200

    added ddl sql

---
+++ db/set_up/ddl.sql
@@ -0,0 +1,57 @@
+CREATE TABLE paper_types
+(
+    id   serial PRIMARY KEY,
+    name varchar(60) NOT NULL
+);
+
+CREATE TABLE paper_formats
+(
+    id   serial PRIMARY KEY,
+    name varchar(60) NOT NULL
+);
+
+CREATE TABLE binding_types
+(
+    id   serial PRIMARY KEY,
+    name varchar(60) NOT NULL
+);
+
+CREATE TABLE countries
+(
+    id   serial PRIMARY KEY,
+    name varchar(30) NOT NULL
+);
+
+CREATE TABLE brands
+(
+    id         serial PRIMARY KEY,
+    name       varchar(60) NOT NULL,
+    country_id integer references countries
+);
+
+CREATE TABLE papers
+(
+    id                  serial PRIMARY KEY,
+    name                varchar(60) NOT NULL,
+    description         text        NOT NULL,
+    pieces              integer     NOT NULL,
+    paper_type_id       integer references paper_types,
+    density             float4      NOT NULL,
+    paper_format_id     integer references paper_formats,
+    brand_id            integer references brands,
+    binding_type_id     integer references binding_types,
+    manufacture_country integer references countries,
+    status              varchar(30) default 'new'
+);
+
+CREATE TABLE colors
+(
+    id   serial PRIMARY KEY,
+    name varchar(20) NOT NULL
+);
+
+CREATE TABLE papers_colors
+(
+    paper_id integer references papers,
+    color_id integer references colors
+);
