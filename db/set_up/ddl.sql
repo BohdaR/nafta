d460a7215451c831d5ddc53fb27f2981fae462b9
BohdaR <bohdanshushval@gmail.com>
2022-12-12 14:47:09 +0200

    changed ddl

--- db/set_up/ddl.sql
+++ db/set_up/ddl.sql
@@ -1,57 +1,44 @@
-CREATE TABLE paper_types
+CREATE TABLE IF NOT EXISTS paper_types
 (
     id   serial PRIMARY KEY,
     name varchar(60) NOT NULL
 );
 
-CREATE TABLE paper_formats
+CREATE TABLE IF NOT EXISTS paper_formats
 (
     id   serial PRIMARY KEY,
     name varchar(60) NOT NULL
 );
 
-CREATE TABLE binding_types
+CREATE TABLE IF NOT EXISTS binding_types
 (
     id   serial PRIMARY KEY,
     name varchar(60) NOT NULL
 );
 
-CREATE TABLE countries
+CREATE TABLE IF NOT EXISTS countries
 (
     id   serial PRIMARY KEY,
     name varchar(30) NOT NULL
 );
 
-CREATE TABLE brands
+CREATE TABLE IF NOT EXISTS brands
 (
     id         serial PRIMARY KEY,
     name       varchar(60) NOT NULL,
     country_id integer references countries
 );
 
-CREATE TABLE papers
+CREATE TABLE IF NOT EXISTS papers
 (
-    id                  serial PRIMARY KEY,
-    name                varchar(60) NOT NULL,
-    description         text        NOT NULL,
-    pieces              integer     NOT NULL,
-    paper_type_id       integer references paper_types,
-    density             float4      NOT NULL,
-    paper_format_id     integer references paper_formats,
-    brand_id            integer references brands,
-    binding_type_id     integer references binding_types,
-    manufacture_country_id integer references countries,
-    status              varchar(30) default 'new'
-);
-
-CREATE TABLE colors
-(
-    id   serial PRIMARY KEY,
-    name varchar(20) NOT NULL
-);
-
-CREATE TABLE papers_colors
-(
-    paper_id integer references papers,
-    color_id integer references colors
+    id              serial PRIMARY KEY,
+    name            varchar(60) NOT NULL,
+    description     text        NOT NULL,
+    pieces          integer     NOT NULL,
+    paper_type_id   integer references paper_types,
+    density         float4      NOT NULL,
+    paper_format_id integer references paper_formats,
+    brand_id        integer references brands,
+    binding_type_id integer references binding_types,
+    country_id      integer references countries
 );
