22922b6105128fcec4311897a41d38eec97a4864
BohdaR <bohdanshushval@gmail.com>
2022-12-13 12:47:55 +0200

    changed dml

---
+++ db/set_up/dml.sql
@@ -0,0 +1,14 @@
+INSERT INTO countries (name)
+VALUES ('China'),
+       ('Ukraine'),
+       ('Poland'),
+       ('Canada'),
+       ('Spain'),
+       ('Germany');
+
+INSERT INTO brands (name, country_id)
+VALUES ('Buromax', 9),
+       ('Kite', 13),
+       ('Cool For School', 13),
+       ('Galeria Papieru', 10);
+
